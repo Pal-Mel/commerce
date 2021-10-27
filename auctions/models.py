91a0b7bea2a10a39b9dd3481a23b12ad4c8afebf
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2021-10-27 16:46:47 +0300

    add migration auction

--- auctions/models.py
+++ auctions/models.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.deletion import DO_NOTHING
 from django.db.models.fields import related
+from django.conf import settings
+
 
 class Categori(models.Model):
     name=models.CharField(max_length=50)
@@ -20,17 +21,17 @@ class Auction(models.Model):
     categori=models.ForeignKey(Categori,on_delete=models.PROTECT,related_name="categories")
     description=models.TextField()
     first_rate=models.FloatField()
-    image=models.ImageField(upload_to='images/', null=True, blank=True)
+    image=models.ImageField(upload_to='images_auctions/%Y/%m/%d', null=True, blank=True)
     status=models.ForeignKey(Status,on_delete=models.PROTECT,related_name="auction_statuses")
     createdate= models.DateTimeField()
-    user=models.CharField(max_length=150) #ManyToOneRel(User, field_name="'id'", to="'id'")
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name }, початкова вартість {self.first_rate} ({self.createdate}) - {self.status}"
 
 class Rates(models.Model):
     auction=models.ForeignKey(Auction,on_delete=models.DO_NOTHING,related_name="rate_auctions")
-    user=models.CharField(max_length=150)#ManyToManyField(User,blank=False,related_name="user_rates")
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     rate=models.FloatField()
     date=models.DateTimeField()
 
@@ -41,7 +42,7 @@ class Comments(models.Model):
     auction=models.ForeignKey(Auction,on_delete=models.DO_NOTHING,related_name="auction_comments")
     comment=models.TextField(max_length=350)
     date=models.DateTimeField()
-    user=models.CharField(max_length=150) #ManyToManyField(User,blank=False)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.auction } - {self.comment} ({self.date, self.user})"
